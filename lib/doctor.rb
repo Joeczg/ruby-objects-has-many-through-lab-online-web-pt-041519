de8bf61c8952a2f87b385a594cf1edaebe22c877
zhenggao cai <caizhengao1992@gmail.com>
2019-05-26 06:03:26 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,25 +1,32 @@
-class Doctor 
-  attr_accessor :name 
-  @@all = []
+class Doctor
+
+  attr_reader :name, :appointment, :patient
+
+  @@all_doc = []
+
   def initialize(name)
-    @name = name 
-    @@all << self 
+    @name = name
+    @@all_doc << self
   end
+
   def self.all
-    @@all
+    @@all_doc
   end
-  
-  def new_appointment(date,patient)
-    Appointment.new(patient,self,date)
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
   end
+
   def appointments
     Appointment.all.select do |appointment|
-      appointment.doctor ==self
+      appointment.doctor == self
     end
   end
+
   def patients
     appointments.map do |appointment|
       appointment.patients
     end
   end
+
 end
